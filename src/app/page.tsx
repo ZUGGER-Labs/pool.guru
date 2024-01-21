d021b890e6474a3ff8fbabc0b5edd05c32b06d3c
guotie <guotie.9@gmail.com>
2024-01-21 19:45:11 +0800

    Head组件 Calcultor 页面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image'
 import { PageProps } from "@/lib/page";
-import { Pools } from '@/components/Pools/Pools';
+import Calculator from '@/components/Calculator/Calculator';
+import { Token } from "@/interfaces/uniswap.interface";
 
 export default function Home(pageProps: PageProps) {
+  const tokens: Token[] = [] // getTokens()
+  
   return (
-    <main className="flex flex-col items-center justify-between p-24 max-w-[1600px] m-auto">
-      <Pools />
+    <main className="flex flex-col items-center justify-between p-24 md:max-w-[1600px] m-auto">
+      <Calculator tokens={tokens} />
     </main>
   )
 }
