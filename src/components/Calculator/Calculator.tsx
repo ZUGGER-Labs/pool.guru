3661cc7d4d2f54b2c7b7291d222291ba93afba93
techhyz <1183648559@qq.com>
2024-02-11 12:56:19 +0800

    code update for caculator

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -73,78 +73,82 @@ function Calculator({ tokens }: { tokens: Token[] }) {
       </div>
 
       <div className="mx-auto w-full md:w-[43rem] mt-4">
-        <div className="">
-          <p className="font-bold leading-5 mb-2">Assets</p>
-          <div className="flex flex-row items-center">
-            <AutoComplete
-              options={tokens}
-              emptyMessage="No results found"
-              onValueChange={handleAddAsset}
-            />
-            <div className="mx-6">
-              <AddIcon />
-            </div>
-            <div className="relative flex items-center">
-              <span className="absolute w-6 text-center text-[#B1B1B1]">$</span>
-              <span>
-                <input
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                  className="w-[150px] border focus-visible:outline-none pl-6 leading-[48px] border-[#B7B1A6] focus:border focus:border-black"
-                  type="number"
-                />
-              </span>
-            </div>
+        <div className="flex flex-row">
+          <p className="font-bold leading-5 mb-2 text-left  w-[538px]">Assets</p>
+          <p className="font-bold leading-5 mb-2">Deposit Amount</p>
+        </div>
+        <div className="flex flex-row items-center">
+          <AutoComplete
+            options={tokens}
+            emptyMessage="No results found"
+            onValueChange={handleAddAsset}
+          />
+          <div className="mx-6">
+            <AddIcon />
+          </div>
+
+          <div className="relative flex items-center">
+            <span className="absolute w-6 text-center text-[#B1B1B1]">$</span>
+            <span>
+              <input
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                className="w-[150px] border focus-visible:outline-none pl-6 leading-[48px] border-[#B7B1A6] focus:border focus:border-black"
+                type="number"
+              />
+            </span>
           </div>
         </div>
 
-        { selectedAssets.size > 0 ? <div className="mt-6">
-          <p className="font-bold leading-5 mb-2">Selected assets</p>
-          <div className="flex flex-col items-center gap-2">
-            {[...selectedAssets].map((asset, index) => (
-              <div
-                key={asset.id}
-                className="flex w-[688px] p-4 border border-black bg-white items-center gap-6"
-              >
-                <div className="flex flex-col items-center justify-center gap-2 w-[90px]">
-                  <div className="flex flex-row items-center gap-2">
-                    <img
-                      src={asset.logoURI}
-                      className="w-6 h-6"
-                      alt="Icon"
-                    ></img>
-                    <span className="flex flex-row text-base font-semibold w-full">
-                      {asset.symbol}
+        {selectedAssets.size > 0 ? (
+          <div className="mt-6">
+            <p className="font-bold leading-5 mb-2">Selected assets</p>
+            <div className="flex flex-col items-center gap-2">
+              {[...selectedAssets].map((asset, index) => (
+                <div
+                  key={asset.id}
+                  className="flex w-[688px] p-4 border border-black bg-white items-center gap-6"
+                >
+                  <div className="flex flex-col items-center justify-center gap-2 w-[90px]">
+                    <div className="flex flex-row items-center gap-2">
+                      <img
+                        src={asset.logoURI}
+                        className="w-6 h-6"
+                        alt="Icon"
+                      ></img>
+                      <span className="flex flex-row text-base font-semibold w-full">
+                        {asset.symbol}
+                      </span>
+                    </div>
+                    <div className="flex rounded-full bg-gray-200 h-28px px-2 items-center">
+                      <span className="text-center text-base italic font-light whitespace-nowrap">
+                        {asset.poolCount} pools
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-center justify-center gap-2 w-[90px]">
+                    <span className="text-gray-400 text-base">Price</span>
+                    <span className="text-base">2.45K</span>
+                  </div>
+                  <div className="flex flex-col items-center justify-center gap-2 w-[90px]">
+                    <span className="text-gray-400 text-base">Change 7d</span>
+                    <span className="text-green-500 text-base"> + 4.12% </span>
+                  </div>
+                  <div className="flex flex-col items-center justify-center gap-2 w-[270px]">
+                    <span className="text-gray-400 text-base">Trend 7d</span>
+                    <span className="text-green-500 text-center h-7">
+                      {" "}
+                      + 4.12%{" "}
                     </span>
                   </div>
-                  <div className="flex rounded-full bg-gray-200 h-28px px-2 items-center">
-                    <span className="text-center text-base italic font-light whitespace-nowrap">
-                      {asset.poolCount} pools
-                    </span>
-                  </div>
+                  <button onClick={() => handleRemoveAsset(asset)}>
+                    <img src="/Rounded-edge.svg" className="w-6 h-6"></img>
+                  </button>
                 </div>
-                <div className="flex flex-col items-center justify-center gap-2 w-[90px]">
-                  <span className="text-gray-400 text-base">Price</span>
-                  <span className="text-base">2.45K</span>
-                </div>
-                <div className="flex flex-col items-center justify-center gap-2 w-[90px]">
-                  <span className="text-gray-400 text-base">Change 7d</span>
-                  <span className="text-green-500 text-base"> + 4.12% </span>
-                </div>
-                <div className="flex flex-col items-center justify-center gap-2 w-[270px]">
-                  <span className="text-gray-400 text-base">Trend 7d</span>
-                  <span className="text-green-500 text-center h-7">
-                    {" "}
-                    + 4.12%{" "}
-                  </span>
-                </div>
-                <button onClick={() => handleRemoveAsset(asset)}>
-                  <img src="/Rounded-edge.svg" className="w-6 h-6"></img>
-                </button>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div> : null}
+        ) : null}
 
         <button className="flex w-[688px] mt-6 p-2.5 justify-center items-center border-2 border-black bg-yellow-300">
           <span className="text-center text-base font-bold">Calculate</span>
