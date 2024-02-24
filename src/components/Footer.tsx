4f44466ec2b6c0c6e5c0c8723230b12144c7142f
techhyz <1183648559@qq.com>
2024-02-24 18:58:41 +0800

    tiny change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,20 +16,14 @@ const Footer = () => {
     <footer className="flex md:mx-auto md:container py-8 justify-between items-center">
       <div className="flex items-center gap-8">
         {LinkItem("FAQ", "https://www.google.com")}
-        {LinkItem("Docs", "https://www.github.com")}
-        {LinkItem("Blog", "https://www.example.com")}
+        {LinkItem("Docs", "https://www.google.com")}
+        {LinkItem("Blog", "https://www.google.com")}
       </div>
       <div className="flex items-start gap-6">
-        {IconItem("/twitter.svg", "https://www.google.com")}
-        {IconItem("/github.svg", "https://www.google.com")}
+        {IconItem("/twitter.svg", "https://twitter.com/ZuggerLabs")}
+        {IconItem("/github.svg", "https://github.com/ZUGGER-Labs")}
         {IconItem("/discord.svg", "https://www.google.com")}
       </div>
-      {/* <div className="flex flex-col justify-center text-slate-500 items-center">
-        <p className="md:text-sm">
-          Copyright © {year === 2024 ? "2024" : `2024 - ${year}`} All Rights
-          Reserved
-        </p>
-      </div> */}
     </footer>
   );
 };
